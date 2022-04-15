36a2ff8763db7536786a28a44f5536a6b8a0f6d2
teppy <tepong32@gmail.com>
2022-04-16 02:04:45 +0800

    initial commit but still working on blog-tags as it shows 404

---
+++ portfoliosite/portfoliosite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-56)o*-291r^w@yvtg8gu21mc7_q%^4vpj9_d8fa=6j@+#-!dys'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
